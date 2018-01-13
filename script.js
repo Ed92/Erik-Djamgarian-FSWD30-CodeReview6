d129405461d5a4fbd9cce02b38b3ed5492d7c6f4
erik djamgarian <djamgarian0702@gmail.com>
2018-01-13 15:20:50 +0100

    First push, pretty much done

---
+++ script.js
@@ -0,0 +1,113 @@
+{
+
+    "Books": [{
+
+        "Title": "Woodwalkers",
+
+        "Author": "Katja Brandis",
+
+        "Genre": "Belletristik",
+
+        "Publisher": "Bild",
+
+        "Rating": "5"
+
+    }, {
+
+        "Title": "Foundation",
+
+        "Author": "Isaac Asimov",
+
+        "Genre": "Fantasy science fiction"
+
+        "Publisher": "Heyne",
+
+        "Rating": "3"
+
+    }]
+
+}
+
+{
+
+    "Comics": [{
+
+        "Title": "The Sandman",
+
+        "Author": "Neil Gaiman",
+
+        "Genre": "Fantasy",
+
+        "Publisher": "Vertigo"
+
+    }, {
+
+        "Title": "Modesty Blaise",
+
+        "Author": "Peter O'Donnell",
+
+        "Genre": "Mystery"
+
+        "Publisher": "Titan Books"
+
+    }]
+
+}
+
+{
+
+    "CDs": [{
+
+        "Title": "Dylan",
+
+        "Author": "Bob Dylan",
+
+        "Genre": "Rock",
+
+        "Minutes": "34"
+
+    }, {
+
+        "Title": "Diamond Life",
+
+        "Author": "Sade",
+
+        "Genre": "Smooth soul"
+
+        "Publisher": "Epic",
+
+        "Minutes": "45"
+
+    }]
+
+}
+
+{
+
+    "Movies": [{
+
+        "Title": "City lights",
+
+        "Author": "Charlie Chaplin",
+
+        "Genre": "Romantic comedy",
+
+        "Publisher": "United Artists",
+
+        "Minutes": "87"
+
+    }, {
+
+        "Title": "Lawrence of Arabia",
+
+        "Author": "T.E.Lawrence",
+
+        "Genre": "Historical drama"
+
+        "Publisher": "Columbia Pictures",
+
+        "Minutes": "222"
+
+    }]
+
+}
